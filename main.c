80c15d8d3af0b7a86369e029fedd4faf7ae78731
Daniru2007 <daniruayuka@gmail.com>
2024-01-05 07:05:14 +0530

    basic web server created following some tutors

---
+++ main.c
@@ -0,0 +1,63 @@
+#include "netinet/in.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "sys/socket.h"
+#include "unistd.h"
+#include <asm-generic/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORT 8000
+
+int main() {
+  int server_fd, new_socket;
+  ssize_t valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  socklen_t addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "HTTP/1.1 200 OK\r\nContent-Type: "
+                "text/html\r\n\r\n<html><body><h1>Hi</h1></body></html>";
+
+  printf("creating socket...\n");
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket not created\n");
+    exit(1);
+  }
+
+  printf("setting options\n");
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                 sizeof(opt))) {
+    perror("socket options not setted\n");
+    exit(1);
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+  printf("binding the server to PORT 8000\n");
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  while (1) {
+    printf("listening\n");
+    if (listen(server_fd, 3) < 0) {
+      perror("listen");
+      exit(EXIT_FAILURE);
+    }
+    printf("accepting\n");
+    if ((new_socket =
+             accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024 - 1);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello send to the client\n");
+
+    close(new_socket);
+  }
+  close(server_fd);
+}
